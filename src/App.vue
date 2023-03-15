893263ba9799aab4880b2e981a5055fe60a1b667
DanieleTuttolani <danieletuttolani2@gmail.com>
2023-03-15 14:34:58 +0100

    incoming api

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+const appUrl = 'http://127.0.0.1:8000'
+export default {
+  data() {
+    return {
+      projects: [],
+      indexUri: '/api/index',
+    }
+  }
+  ,
+  methods: {
+    fetchProjectIndex() {
+      axios.get(appUrl + this.indexUri)
+        .then(res => {
+          this.projects = res.data
+        })
+    }
+  }
+  ,
+  mounted() {
+    this.fetchProjectIndex()
+  },
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>vite di daniele</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
